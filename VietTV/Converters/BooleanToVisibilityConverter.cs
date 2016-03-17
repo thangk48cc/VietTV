352f85af40610f1ae5e6e5e289b693f02864159a
Vu Dinh Luan <vdluan.ictu@gmail.com>
2016-03-18 00:19:35 +0700

    d

---
+++ VietTV/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace VietTV.Converters
+{
+    public sealed class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //if (value != null)
+            //{
+            //    if (value.ToString().ToLower() == "true") return Visibility.Visible;
+            //    if (value.ToString().ToLower() == "false") return Visibility.Collapsed;
+            //}
+            return (value is bool && (bool) value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility && (Visibility) value == Visibility.Visible;
+        }
+    }
+}
---
+++ VietTV/View/PageMainPanel.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using VietTV.Model;
+using VietTV.ViewModel;
+
+namespace VietTV.View
+{
+    public partial class PageMainPanel : PhoneApplicationPage
+    {
+        public PageMainPanel()
+        {
+            InitializeComponent();
+            var vm = DataContext as MenuMainVM;
+            vm.getDataFromServiceCommand.Execute(null);
+            stbCloseMenu.Completed += stbCloseMenu_Completed;
+            stbOpenMenu.Completed += stbOpenMenu_Completed;
+        }
+
+        void stbOpenMenu_Completed(object sender, EventArgs e)
+        {
+            isOpen = true;
+        }
+
+        private bool isOpen = false;
+        void stbCloseMenu_Completed(object sender, EventArgs e)
+        {
+            isOpen = false;
+        }
+
+        void MenuSetting()
+        {
+            if (isOpen)
+            {
+                stbCloseMenu.Begin();
+                btnMenuMain.Background = new SolidColorBrush(Color.FromArgb(255, 0, 154, 216));
+            }
+            else
+            {
+                stbOpenMenu.Begin();
+                btnMenuMain.Background = new SolidColorBrush(Color.FromArgb(255,79,184,229));
+            }
+        }
+        private void BtnMenuMain_OnClick(object sender, RoutedEventArgs e)
+        {
+            MenuSetting();
+        }
+
+        private void lstChanelsMenu_SelectedChange(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (GetListChanels) sender;
+            var vm = DataContext as MenuMainVM;
+            vm.chanelsByGroup = item.chanels;
+        }
+
+        private void BtnItemGroupChanel_OnClick(object sender, RoutedEventArgs e)
+        {
+            var item = (GetListChanels)(sender as Button).DataContext;
+            var vm = DataContext as MenuMainVM;
+            vm.chanelsByGroup = item.chanels;
+            MenuSetting();
+        }
+    }
+}
